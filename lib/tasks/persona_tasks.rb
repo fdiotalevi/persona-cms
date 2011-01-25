d233d7d5d57ce37717566ce357530166a24bdca2
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2011-01-26 00:01:09 +0100

    completed cli

--- lib/tasks/persona_tasks.rb
+++ lib/tasks/persona_tasks.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 class PersonaTasks
   
   def self.create_site(base_dir, site_name)
-    FileUtils.cp_r '../template', base_dir
+    FileUtils.cp_r(File.join(File.dirname(__FILE__), "..", "..", "template"), site_name)
   end
   
   
